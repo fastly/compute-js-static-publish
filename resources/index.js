721695f4865e7835aeeafa591df1ae8d843ff906
Katsuyuki Omuro <komuro@fastly.com>
2023-03-09 19:06:30 +0900

    Don't lazily instantiate the PublisherServer in index.js

--- resources/index.js
+++ resources/index.js
@@ -1,12 +1,12 @@
 /// <reference types="@fastly/js-compute" />
 
 import { getServer } from './statics.js';
+const server = getServer();
 
 // eslint-disable-next-line no-restricted-globals
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 async function handleRequest(event) {
 
-  const server = getServer();
   const response = await server.serveRequest(event.request);
 
   if (response != null) {
